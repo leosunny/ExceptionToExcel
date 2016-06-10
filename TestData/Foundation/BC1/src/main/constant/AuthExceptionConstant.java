0e92aa7cecc0c4ba147598021c3e6c45dd2ee292
leosunny <hhu_gis@qq.com>
2016-06-10 14:48:04 +0800

    add test data

---
+++ TestData/Foundation/BC1/src/main/constant/AuthExceptionConstant.java
@@ -0,0 +1,5 @@
+public static final string USERID_NULL="60101030001";
+
+public static final string ROLEID_NULL="60101030002";
+
+public static final string AUTHID_NULL="60101030003";
---
+++ TestData/Foundation/BC1/src/main/java/Helloworld.java
@@ -0,0 +1,3 @@
+BesException exception=new BesException(AuthExceptionConstant.USERID_NULL);
+
+BesException exception2=new BesException(AuthExceptionConstant.ROLEID_NULL,e);
